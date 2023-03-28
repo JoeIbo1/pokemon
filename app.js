41040db464a27661c96a1585cd282f25ef42b76e
JoeIbo1 <josef5222@hotmail.com>
2023-03-28 12:00:20 +0200

    almost there

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
-  const machoke = await getCharacter("https://raw.githubusercontent.com/JoeIbo1/pokemon/main/jason/pok.json");
-  showCharacter(machoke);
-  showInformation(machoke);
+  const machoke = await getCharacter("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
+  machoke.forEach(showCharacter);
+ 
 
 
 
@@ -18,69 +18,61 @@ async function getCharacter(url) {
 }
 
 function showCharacter(character) {
-  document.querySelector("body").insertAdjacentHTML(
+  console.log(character);
+
+  document.querySelector("#characters").insertAdjacentHTML(
     "beforeend",
     /*html*/ `
-        
-        <article>
-        <h2>${character.name}</h2>
-        <img src= "${character.image}"/>
-        </article>
-        
+            <article class="grid-item">
+                <img src="${character.image}">
+                <h2>${character.name}</h2>
+                <p>Generation: ${character.generation}</p>
+                <p>Spil Version: ${character.spilVersion}</p>
+                <p>Type: ${character.type}</p>
+            </article>
         `
   );
+  document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
+
+  function characterClicked() {
+    showCharacterModal(character);
+  }
 }
 
-// "use strict";
+function showCharacterModal(character) {
+  console.log(character);
+  //image, name and house
+  document.querySelector("#dialog-image").src = character.image;
+  document.querySelector("#dialog-name").textContent = character.name;
+  document.querySelector("#dialog-description").textContent = character.description;
+  // description
+  let description = generateDescription(character);
 
-// const machoke = {
-//   name: "Machoke ",
-//   description: "Its muscular body is so powerful, it must wear a power-save belt to be able to regulate its motions",
-//   ability: "Its so gutsy that having a status condition boosts the Pokémon’s Attack stat.",
-//   image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/067.png",
-//   footprint: "https://archives.bulbagarden.net/media/upload/8/87/F0164gen2.png",
-//   dexindex: "#0067",
-//   type: "Fighting",
-//   subtype: "Fighting",
-//   weaknesses: "Psychic, Flying, Fairy.",
-//   gender: "both",
-//   weight: "155.4 lbs",
-//   height: "149.86 cm",
-//   generation: "Generation 1",
-//   spilversion: "All versions",
-//   canEvolve: "Machamp",
-//   statsHP: "80",
-//   statsAttack: "100",
-//   statsDefence: "70",
-//   statsSpecialAttack: "50",
-//   statsSpecialDefence: "60",
-//   statsSpeed: "45",
-// };
+  document.querySelector("#dialog-gender").textContent = character.gender;
+  document.querySelector("#dialog-generation").textContent = character.generation;
+  document.querySelector("#dialog-height").textContent = character.height;
+  document.querySelector("#dialog-weight").textContent = character.weight;
+  document.querySelector("#dialog-can-evolve").textContent = character.canEvolve;
+  document.querySelector("#dialog-spil-version").textContent = character.spilVersion;
+  document.querySelector("#dialog-dexindex").textContent = character.dexindex;
+  document.querySelector("#dialog-type").textContent = character.type;
+  document.querySelector("#dialog-subtype").textContent = character.subtype;
+  document.querySelector("#dialog-weaknesses").textContent = character.weaknesses;
+  document.querySelector("#dialog-ability").textContent = character.ability;
 
-function showInformation(pokemon) {
-  const myHTML = /*html*/ `
-<li>Name: ${pokemon.name}</li>
-<li>Description: ${pokemon.description}</li>
-<li>Ability: ${pokemon.ability}</li>
-<li>Image: ${pokemon.image}</li>
-<li>Footprint: ${pokemon.footprint}</li>
-<li>Dexindex: ${pokemon.dexindex}</li>
-<li>Type: ${pokemon.type}</li>
-<li>subtype: ${pokemon.subtype}</li>
-<li>Weaknesses: ${pokemon.weaknesses}</li>
-<li>Gender: ${pokemon.gender}</li>
-<li>Weight: ${pokemon.weight}</li>
-<li>Height: ${pokemon.height}</li>
-<li>Generation: ${pokemon.generation}</li>
-<li>Spilversion: ${pokemon.spilversion}</li>
-<li>Can Evolve: ${pokemon.canEvolve}</li>
-<li>HP: ${pokemon.statsHP}</li>
-<li>Attack: ${pokemon.statsAttack}</li>
-<li>Special attack: ${pokemon.statsSpecialAttack}</li>
-<li>Special defence: ${pokemon.statsSpecialDefence}</li>
-<li>Speed: ${pokemon.statsSpeed}</li>
-`;
-  document.querySelector("#pokemons").insertAdjacentHTML("beforeend", myHTML);
-  
+  document.querySelector("#dialog-statshp").textContent = character.statsHP;
+  document.querySelector("#dialog-statsattack").textContent = character.statsAttack;
+  document.querySelector("#dialog-statsdefence").textContent = character.statsDefence;
+  document.querySelector("#dialog-statsspecialattack").textContent = character.statsSpecialAttack;
+  document.querySelector("#dialog-statsspecialdefence").textContent = character.statsSpecialDefence;
+  document.querySelector("#dialog-statsspeed").textContent = character.statsSpeed;
+
+  // show dialog
+  document.querySelector("#dialog-character").showModal();
 }
 
+function generateDescription(character) {
+  let description = "";
+ 
+  return description;
+}

87786079a490b28ca2caae75985fbf2148248121
JoeIbo1 <josef5222@hotmail.com>
2023-03-28 15:42:29 +0200

    done

--- app.js
+++ app.js
@@ -3,76 +3,76 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
-  const machoke = await getCharacter("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
-  machoke.forEach(showCharacter);
+  const pokemons = await getPokemon("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
+  
+  for(const pokemon of pokemons){
+    showPokemon(pokemon);
+  }
+  
  
 
 
 
 }
 
-async function getCharacter(url) {
+async function getPokemon(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
-function showCharacter(character) {
-  console.log(character);
+function showPokemon(pokemon) {
+  console.log(pokemon);
 
-  document.querySelector("#characters").insertAdjacentHTML(
+  document.querySelector("#pokemons").insertAdjacentHTML(
     "beforeend",
     /*html*/ `
             <article class="grid-item">
-                <img src="${character.image}">
-                <h2>${character.name}</h2>
-                <p>Generation: ${character.generation}</p>
-                <p>Spil Version: ${character.spilVersion}</p>
-                <p>Type: ${character.type}</p>
+                <img src="${pokemon.image}">
+                <img src="${pokemon.footprint}">
+                <h2>${pokemon.name}</h2>
+                <p>Generation: ${pokemon.generation}</p>
+                <p>Spil Version: ${pokemon.spilversion}</p>
+                <p>Type: ${pokemon.type}</p>
             </article>
         `
   );
-  document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
+  document.querySelector("#pokemons article:last-child").addEventListener("click", pokemonClicked);
 
-  function characterClicked() {
-    showCharacterModal(character);
+  function pokemonClicked() {
+    showPokemonModal(pokemon);
   }
 }
 
-function showCharacterModal(character) {
-  console.log(character);
-  //image, name and house
-  document.querySelector("#dialog-image").src = character.image;
-  document.querySelector("#dialog-name").textContent = character.name;
-  document.querySelector("#dialog-description").textContent = character.description;
-  // description
-  let description = generateDescription(character);
+function showPokemonModal(pokemon) {
+  console.log(pokemon);
+  //image, name and description
+  document.querySelector("#dialog-image").src = pokemon.image;
+  document.querySelector("#dialog-name").textContent = pokemon.name;
+  document.querySelector("#dialog-description").textContent = pokemon.description;
+  
 
-  document.querySelector("#dialog-gender").textContent = character.gender;
-  document.querySelector("#dialog-generation").textContent = character.generation;
-  document.querySelector("#dialog-height").textContent = character.height;
-  document.querySelector("#dialog-weight").textContent = character.weight;
-  document.querySelector("#dialog-can-evolve").textContent = character.canEvolve;
-  document.querySelector("#dialog-spil-version").textContent = character.spilVersion;
-  document.querySelector("#dialog-dexindex").textContent = character.dexindex;
-  document.querySelector("#dialog-type").textContent = character.type;
-  document.querySelector("#dialog-subtype").textContent = character.subtype;
-  document.querySelector("#dialog-weaknesses").textContent = character.weaknesses;
-  document.querySelector("#dialog-ability").textContent = character.ability;
+  document.querySelector("#dialog-gender").textContent = pokemon.gender;
+  document.querySelector("#dialog-generation").textContent = pokemon.generation;
+  document.querySelector("#dialog-height").textContent = pokemon.height;
+  document.querySelector("#dialog-weight").textContent = pokemon.weight;
+  document.querySelector("#dialog-can-evolve").textContent = pokemon.canEvolve;
+  document.querySelector("#dialog-spil-version").textContent = pokemon.spilversion;
+  document.querySelector("#dialog-dexindex").textContent = pokemon.dexindex;
+  document.querySelector("#dialog-type").textContent = pokemon.type;
+  document.querySelector("#dialog-subtype").textContent = pokemon.subtype;
+  document.querySelector("#dialog-weaknesses").textContent = pokemon.weaknesses;
+  document.querySelector("#dialog-ability").textContent = pokemon.ability;
 
-  document.querySelector("#dialog-statshp").textContent = character.statsHP;
-  document.querySelector("#dialog-statsattack").textContent = character.statsAttack;
-  document.querySelector("#dialog-statsdefence").textContent = character.statsDefence;
-  document.querySelector("#dialog-statsspecialattack").textContent = character.statsSpecialAttack;
-  document.querySelector("#dialog-statsspecialdefence").textContent = character.statsSpecialDefence;
-  document.querySelector("#dialog-statsspeed").textContent = character.statsSpeed;
+  document.querySelector("#dialog-statshp").textContent = pokemon.statsHP;
+  document.querySelector("#dialog-statsattack").textContent = pokemon.statsAttack;
+  document.querySelector("#dialog-statsdefence").textContent = pokemon.statsDefence;
+  document.querySelector("#dialog-statsspecialattack").textContent = pokemon.statsSpecialAttack;
+  document.querySelector("#dialog-statsspecialdefence").textContent = pokemon.statsSpecialDefence;
+  document.querySelector("#dialog-statsspeed").textContent = pokemon.statsSpeed;
 
   // show dialog
-  document.querySelector("#dialog-character").showModal();
+  document.querySelector("#dialog-pokemon").showModal();
 }
 
-function generateDescription(character) {
-  let description = "";
- 
-  return description;
-}
+
